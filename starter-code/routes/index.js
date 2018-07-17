6a79358bf84756f24e0c6f8a4b8e3dcc7e0bd83f
Bruno <brunodomecq@gmail.com>
2018-07-17 16:29:04 +0200

    cambios en el usuario

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -5,6 +5,8 @@ const multer = require("multer")
 const upload = multer ({dest:"./public/uploads/"})
 const Picture = require("../models/Picture")
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
+const bcryptSalt = 10
 /* GET home page */
 router.get('/explore',ensureLoggedIn("/auth/login"), (req, res, next) => {
   Picture.find().sort({updated_at: 1}).then((Pictures)=>{
@@ -23,6 +25,50 @@ router.get('/collect', ensureLoggedIn("/auth/login"),(req, res, next) => {
 router.get('/userinfo',ensureLoggedIn("/auth/login"), (req, res, next) => {
     res.render('user-info');
 });
+
+/* router.post('/userinfo' , (req,res,next) => {
+  const newInfo = {
+    email: req.body.email,
+    name: req.body.username,
+  }
+
+  User.findOneAndUpdate({username:user.username})
+  .then(()=>{
+
+    newInfo.save();
+    res.redirect('/');
+  })
+ 
+
+
+}) */
+
+
+router.post("/userinfo",(req,res,next) =>{
+  const user = req.user;
+  const {username,email,password,passconfirm}=req.body;
+  // console.log(req.file)
+  // const imgPath = req.file;
+  // if(imgPath==null || imgPath==undefined){
+  //   imgPath="https://static.vix.com/es/sites/default/files/styles/large/public/btg/universos-2.jpg?itok=IpTWZVlD"
+  // }
+  if (password !=passconfirm ) {
+    res.render("user-info", { message: "Wrong password" ,user});
+  }
+  else if(password){
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+  User.findOneAndUpdate({_id:user},{"username":username,"email": email,"password":hashPass}).then(()=>{
+   res.redirect("/");
+  })
+}
+  else{
+  User.findOneAndUpdate({_id:user},{"username":username,"email":email}).then(()=>{
+    res.redirect("/");
+  });
+  }
+ });
+
 
 router.get('/mycollection', ensureLoggedIn("/auth/login"),(req, res, next) => {
   User.findById(req.user._id)
