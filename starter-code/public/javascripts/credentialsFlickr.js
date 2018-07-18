00b9b10df524aa4d5a5a261c7faf337d36e244b6
Niko <nkouuu@hotmail.com>
2018-07-18 17:02:44 +0200

    añadir y borrar albums, fotos...

--- starter-code/public/javascripts/credentialsFlickr.js
+++ starter-code/public/javascripts/credentialsFlickr.js
@@ -1,5 +1,2 @@
 var api_key = '3560756fc8c701f581823d68c06d05ac';
 var user_id = '98d3cf1c16a52253';
-var i_user_id = "daf119e5e9154df5995c28eb08d087bd"
-var i_secret = "05b5b86fc04f439091b3d2e2161979c5"
-var i_token ="319189465.daf119e.795acd35cbcf41168f0d068651b0d024"
